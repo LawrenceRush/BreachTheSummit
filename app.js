b6a718a585913280b8b1b0cbcb08478c5dc439cd
Joseph <jandrade008@gmail.com>
2019-10-04 13:38:49 -0700

    change

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ function testAPI(){
   console.log("Longitute is " + lon);
   useHikingApi(lat, lon)
   console.log(longitute);
-
+  initMap();
 
 })
 
@@ -61,7 +61,7 @@ function useHikingApi(x, y) {
     var center = new google.maps.LatLng(latitude,longitute);
     map.panTo(center);
         
-
+    
         
   })
 }
@@ -76,10 +76,9 @@ function initMap() {
   // The location of Uluru
   var uluru = { lat: latitude, lng: longitute };
   // The map, centered at Uluru
-  map = new google.maps.Map(
-      document.getElementById('map'), { zoom: 4, center: uluru });
+  map = new google.maps.Map(document.getElementById('map'), { zoom: 4, center: uluru });
   // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({ position: uluru, map: map });
+  var marker = new google.maps.Marker({ zoom: 7, position: uluru, map: map});
     
 }
 
